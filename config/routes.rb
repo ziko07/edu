8b4c2834b0a32cd772c0d584b2300ac051fa07c7
plabon-asad <asad.ku.math@gmail.com>
2016-11-19 17:50:35 +0600

    Music Gallary Design

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get 'category_view' => 'welcome#category_view'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
