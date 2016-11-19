544afef2cfa47024a99d8f08d005b211f2511668
Nazrul <nazrulku07@gmail.com>
2016-11-19 18:04:07 +0600

    add category images and create course model

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,10 @@
+class CoursesController < ApplicationController
+  def index
+
+  end
+
+  def category_courses
+    
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118180755) do
+ActiveRecord::Schema.define(version: 20161119105302) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
