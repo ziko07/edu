0a5a31119a8dedb6f0010b5be2742bb0baed23f5
raihan31 <raihan.rubel.31@gmail.com>
2016-11-22 20:56:13 +0600

    ajax registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122044645) do
+ActiveRecord::Schema.define(version: 20161122115026) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20161122044645) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "full_name",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
