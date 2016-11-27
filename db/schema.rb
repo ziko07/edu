2007d8c5ad14ac3630be0008625e2c8108a18b1e
Nazrul <nazrulku07@gmail.com>
2016-11-27 23:01:07 +0600

    fixed course create design issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161127122637) do
+ActiveRecord::Schema.define(version: 20161127150214) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.string   "image",      limit: 255
     t.string   "slug",       limit: 255
     t.integer  "position",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "course_statuses", force: :cascade do |t|
+    t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
@@ -29,18 +35,11 @@ ActiveRecord::Schema.define(version: 20161127122637) do
     t.string   "subtitle",         limit: 255
     t.integer  "category_id",      limit: 4
     t.integer  "user_id",          limit: 4
-    t.text     "summary",          limit: 65535
     t.string   "image",            limit: 255
+    t.text     "summary",          limit: 65535
     t.string   "promo_video",      limit: 255
     t.string   "slug",             limit: 255
     t.integer  "course_status_id", limit: 4,     default: 1
-    t.string   "title",       limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.string   "subtitle",    limit: 255
-    t.integer  "category_id", limit: 4
-    t.integer  "user_id",     limit: 4
-    t.string   "image",       limit: 255
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
